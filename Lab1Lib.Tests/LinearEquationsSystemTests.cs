5da2b0b8d3691b2c1951b238b6682bbe5e68b9e9
GnomGad <canimore@yandex.ua>
2021-05-04 20:10:55 +0300

    add LinearEquationSystem

---
+++ Lab1Lib.Tests/LinearEquationsSystemTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+namespace Lab1Lib.Tests
+{
+    [TestFixture, Description("Покрытие тестами класса системы линейных уравнений")]
+    class LinearEquationsSystemTests
+    {
+
+        static object[] NotCorrectSizeMatrix =
+        {
+            new double[,] { { 1}, { 1} },
+            new double[,] { { 12, 3 }, { 4,2 } },
+            new double[,] {{5,3},{1,5}, { 1, 5 } , { 1, 5 } , { 1, 5 } }
+        };
+        static object[] CorrectSizeMatrix =
+        {
+            new double[,] { { 1, 1,5 }, { 1,1,3 } },
+            new double[,] { { 12, 3,1 }, { 4,2,6 } },
+            new double[,] {{5,3,1,4},{1,5,2,3}, { 1, 5,5,1 } }
+        };
+        static object[] ZeroDeterminantMatrix =
+{
+            new double[,] { { 1, 1,3 }, { 1,1,1 } },
+            new double[,] { { 5, 5,5,2 }, { 5, 5,5,5 },{ 5, 5,5,8 }, },
+        };
+
+        LinearEquationsSystem linear;
+
+        [SetUp]
+        public void Setup()
+        {
+            linear = new LinearEquationsSystem();
+        }
+
+        [Description("Задан неправильная матрица, исходя из этого должна происходить ошибка")]
+        [TestCaseSource(nameof(NotCorrectSizeMatrix))]
+        public void SetCoefficients_NotCorrectSizeMatrix_FormatException(double[,] d)
+        {
+            Assert.Throws<FormatException>(() => { linear.SetCoefficients(d); });
+        }
+
+        [Description("Задан неправильная матрица, исходя из этого должна происходить ошибка")]
+        [TestCaseSource(nameof(NotCorrectSizeMatrix))]
+        public void LinearEquationsSystemConstructor_NotCorrectSizeMatrix_FormatException(double[,] d)
+        {
+            Assert.Throws<FormatException>(() => { linear = new LinearEquationsSystem(d); });
+        }
+
+        [Description("Задан матрица с нулевым определителем, исходя из этого должна происходить ошибка")]
+        [TestCaseSource(nameof(ZeroDeterminantMatrix))]
+        public void SetCoefficients_ZeroDeterminantMatrix_ArgumentException(double[,] d)
+        {
+            var l = new LinearEquationsSystem();
+            Assert.Throws<ArgumentException>(() => { l.SetCoefficients(d); });
+        }
+
+        [Description("3х3 Матрица и её решение")]
+        [Test]
+        public void Solve_3x3Matrix_Solved()
+        {
+            double[,] c = { { 2, 2, 2, 20 }, { 4, 6, 2, 40 }, { 4, 16, 26, 72 } };
+            double[] a = { (double)138 / 17, (double)16 / 17, (double)16 / 17 };
+            linear.SetCoefficients(c);
+            CollectionAssert.AreEqual(linear.Solve(), a);
+        }
+
+        [Description("2х2 Матрица и её решение")]
+        [Test]
+        public void Solve_2x2Matrix_Solved()
+        {
+            double[,] c = { { 2, 4, 2 }, { 2, 6, 4 } };
+            double[] a = { -1, 1 };
+            linear.SetCoefficients(c);
+            CollectionAssert.AreEqual(linear.Solve(), a);
+        }
+    }
+}
---
+++ Lab1Lib/LinearEquationsSystem.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1Lib
+{
+    public class LinearEquationsSystem
+    {
+        private double[,] _matrix;
+
+        public LinearEquationsSystem()
+        {
+
+        }
+
+        /// <summary>
+        /// Параметризованный конструктор
+        /// </summary>
+        /// <param name="coeffs">массив [n,n], где n == 2 || n == 3</param>
+        public LinearEquationsSystem(double[,] coeffs)
+        {
+            SetCoefficients(coeffs);
+        }
+
+        public void SetCoefficients(double[,] coeffs)
+        {
+            if (!IsCorrectSize(coeffs))
+                throw new FormatException("Bad size");// размерность непонятная
+            if (MatrixDeterminant(coeffs) == (double)0)
+                throw new ArgumentException("Zero determinant");
+
+            _matrix = coeffs;
+
+        }
+
+        /// <summary>
+        /// Проверка размера массива
+        /// </summary>
+        /// <param name="coeffs"></param>
+        /// <returns>true если верно</returns>
+        public bool IsCorrectSize(double[,] coeffs)
+        {
+            if ((coeffs.GetLength(0) == 2 && coeffs.GetLength(1) == 3) ||
+                (coeffs.GetLength(0) == 3 && coeffs.GetLength(1) == 4))
+                return true;
+            else
+                return false;
+
+        }
+
+        /// <summary>
+        /// Просчет детерминанта матрицы
+        /// </summary>
+        /// <param name="coeffs"></param>
+        /// <returns></returns>
+        public double MatrixDeterminant(double[,] coeffs)
+        {
+            if (coeffs.Length == 6)
+                return coeffs[0, 0] * coeffs[1, 1] - coeffs[0, 1] * coeffs[1, 0];
+            return (coeffs[0, 0] * coeffs[1, 1] * coeffs[2, 2]
+           + coeffs[1, 0] * coeffs[2, 1] * coeffs[0, 2]
+           + coeffs[0, 1] * coeffs[1, 2] * coeffs[2, 0]
+           - coeffs[0, 2] * coeffs[1, 1] * coeffs[2, 0]
+           - coeffs[0, 0] * coeffs[2, 1] * coeffs[1, 2]
+           - coeffs[0, 1] * coeffs[1, 0] * coeffs[2, 2]);
+        }
+
+
+        public double[] Solve()
+        {
+            return Gauss(_matrix);
+        }
+
+        /// <summary>
+        /// Метод гаусса взят с википедии
+        /// </summary>
+        /// <param name="Matrix"></param>
+        /// <returns></returns>
+        public double[] Gauss(double[,] Matrix)
+        {
+            int n = Matrix.GetLength(0); //Размерность начальной матрицы (строки)
+            double[,] Matrix_Clone = new double[n, n + 1]; //Матрица-дублер
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n + 1; j++)
+                    Matrix_Clone[i, j] = Matrix[i, j];
+
+            //Прямой ход (Зануление нижнего левого угла)
+            for (int k = 0; k < n; k++) //k-номер строки
+            {
+                for (int i = 0; i < n + 1; i++) //i-номер столбца
+                    Matrix_Clone[k, i] = Matrix_Clone[k, i] / Matrix[k, k]; //Деление k-строки на первый член !=0 для преобразования его в единицу
+                for (int i = k + 1; i < n; i++) //i-номер следующей строки после k
+                {
+                    double K = Matrix_Clone[i, k] / Matrix_Clone[k, k]; //Коэффициент
+                    for (int j = 0; j < n + 1; j++) //j-номер столбца следующей строки после k
+                        Matrix_Clone[i, j] = Matrix_Clone[i, j] - Matrix_Clone[k, j] * K; //Зануление элементов матрицы ниже первого члена, преобразованного в единицу
+                }
+                for (int i = 0; i < n; i++) //Обновление, внесение изменений в начальную матрицу
+                    for (int j = 0; j < n + 1; j++)
+                        Matrix[i, j] = Matrix_Clone[i, j];
+            }
+
+            //Обратный ход (Зануление верхнего правого угла)
+            for (int k = n - 1; k > -1; k--) //k-номер строки
+            {
+                for (int i = n; i > -1; i--) //i-номер столбца
+                    Matrix_Clone[k, i] = Matrix_Clone[k, i] / Matrix[k, k];
+                for (int i = k - 1; i > -1; i--) //i-номер следующей строки после k
+                {
+                    double K = Matrix_Clone[i, k] / Matrix_Clone[k, k];
+                    for (int j = n; j > -1; j--) //j-номер столбца следующей строки после k
+                        Matrix_Clone[i, j] = Matrix_Clone[i, j] - Matrix_Clone[k, j] * K;
+                }
+            }
+
+            //Отделяем от общей матрицы ответы
+            double[] Answer = new double[n];
+            for (int i = 0; i < n; i++)
+                Answer[i] = Matrix_Clone[i, n];
+
+            return Answer;
+        }
+    }
+}
