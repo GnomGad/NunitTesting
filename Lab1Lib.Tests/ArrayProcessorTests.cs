dd637fc64ababf59b0ffe9a259791cccb4ce0ac2
GnomGad <canimore@yandex.ua>
2021-05-05 15:46:21 +0300

    add ArrayProcessor

---
+++ Lab1Lib.Tests/ArrayProcessorTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Lab1Lib.Tests
+{
+    [TestFixture, Description("Тестирование")]
+    class ArrayProcessorTests
+    {
+        ArrayProcessor array;
+        [SetUp]
+        public void Setup()
+        {
+            array = new ArrayProcessor();
+        }
+
+        [Test, Description("Отдать Негативный, получить позитивный ")]
+        public void SortAndFilter_NegativeUnsorted_PositiveSorted()
+        {
+            double[] a = { 10, -5, 3, -8, 0 };
+            double[] b = { 10, 5, 3, 8, 0 };
+            CollectionAssert.AreEquivalent(array.SortAndFilter(a), b);
+        }
+
+        [Test, Description("Получить несортированный массив, отдать сортированный")]
+        public void SortAndFilter_Unsorted_Sorted()
+        {
+            double[] a = { 10, -5, 3, -8, 0, 0, -5, 10, -8, 3, 3, 3, 3, 3, 3, 3 };
+            double[] b = { 10, 5, 3, 8, 0 };
+            CollectionAssert.AreEquivalent(array.SortAndFilter(a), b);
+        }
+    }
+}
---
+++ Lab1Lib/ArrayProcessor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1Lib
+{
+    public class ArrayProcessor
+    {
+        public double[] SortAndFilter(double[] a)
+        {
+            double[] new_a = a.Distinct().ToArray();
+
+            for (int i = 0; i < new_a.Length; i++)
+                new_a[i] = GetChanged(new_a[i]);
+
+            Sort(ref new_a);
+            return new_a;
+        }
+
+        public void Sort(ref double[] a)
+        {
+            double temp;
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                for (int j = i + 1; j < a.Length; j++)
+                {
+                    if (a[i] < a[j])
+                    {
+                        temp = a[i];
+                        a[i] = a[j];
+                        a[j] = temp;
+                    }
+                }
+            }
+        }
+        public double GetChanged(double a) => a < 0 ? -1 * a : a;
+    }
+
+    public class MyComparer : IComparer
+    {
+        public int Compare(object x, object y) => (double)x < (double)y ? 1 : -1;
+    }
+}
+
