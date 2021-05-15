d858872171bb255647de576ecb5ad744eb8b905e
GnomGad <canimore@yandex.ua>
2021-05-15 15:41:53 +0300

    Add Mocks

---
+++ Lab2Lib.Tests/Mocks/MockFileSystemObject.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab2Lib.Tests.Mocks
+{
+    public class MockFileSystemObject : IFileSystemObject
+    {
+        public bool CreateFileWasCalled { get; private set; }
+        public bool DeleteFileWasCalled { get; private set; }
+        public bool ExsistsWasCalled { get; private set; }
+        public bool FileSizeWasCalled { get; private set; }
+        public bool GetFileDataWasCalled { get; private set; }
+        public bool GetFilesWasCalled { get; private set; }
+        public bool ReadLinesWasCalled { get; private set; }
+
+        public bool CreateFile(string name, byte[] data)
+        {
+            CreateFileWasCalled = true;
+            return true;
+        }
+
+        public bool DeleteFile(string path)
+        {
+            DeleteFileWasCalled = true;
+            return true;
+        }
+
+        public bool Exsists(string path)
+        {
+            ExsistsWasCalled = true;
+            return true;
+        }
+
+        public int FileSize(string name)
+        {
+            FileSizeWasCalled = true;
+            return 1;
+        }
+
+        public byte[] GetFileData(string file)
+        {
+            GetFileDataWasCalled = true;
+            return new byte[] { };
+        }
+
+        public string[] GetFiles(string path)
+        {
+            GetFilesWasCalled = true;
+            return new string[] { "File1.tmp", "File2.tmp", "File3.tmp" };
+        }
+
+        public string[] ReadLines(string name)
+        {
+            ReadLinesWasCalled = true;
+            return new string[] { "ssssss", "Hello.txt" };
+        }
+    }
+}
---
+++ Lab2Lib.Tests/Mocks/MockFilseService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab2Lib.Tests.Mocks
+{
+    public class MockFilseService : IFileService
+    {
+        public bool MergeTemporaryFilesWasCalled { get; private set; }
+        public bool RemoveTemporaryFilesWasCalled { get; private set; }
+
+        public int MergeTemporaryFiles(string dir)
+        {
+            MergeTemporaryFilesWasCalled = true;
+            return 0;
+        }
+
+        public int RemoveTemporaryFiles(string dir)
+        {
+            RemoveTemporaryFilesWasCalled = true;
+            return 0;
+        }
+    }
+}
